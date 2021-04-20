13c64f554a0c304e22d144017d43359a6058f73c
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 21:48:11 +0530

    Added 2nd operation a*b+c

--- sortingalgo.sh
+++ sortingalgo.sh
@@ -4,3 +4,5 @@ read -p "Enter 2st value=" b;
 read -p "Enter 3st value=" c;
 opration1=$((a+b*c))
 echo "The output for Opration-1 = "$opration1
+opration2=$((a*b+c))
+echo "The output for Opration-2 = "$opration2
