855be4e1e45a5c4c3a8db176efbf03daffbee9e0
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 22:37:25 +0530

    Added 3rd operation c+a/b

--- sortingalgo.sh
+++ sortingalgo.sh
@@ -1,8 +1,12 @@
 echo "Welcome"
+
 read -p "Enter 1st value=" a;
 read -p "Enter 2st value=" b;
 read -p "Enter 3st value=" c;
-opration1=$((a+b*c))
+opration1=`awk "BEGIN {print $a+($b*$c)}"`
 echo "The output for Opration-1 = "$opration1
-opration2=$((a*b+c))
+opration2=`awk "BEGIN {print ($a*$b)+$c}"`
 echo "The output for Opration-2 = "$opration2
+opration3=`awk "BEGIN{print $c+($a/$b)}"`
+echo "The output for Opration-3 = "$opration3
+
