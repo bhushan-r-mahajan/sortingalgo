75746eae9c6f25baf663821783423d5af840a8c4
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 22:47:03 +0530

    Added 4th operation a%b+c

--- sortingalgo.sh
+++ sortingalgo.sh
@@ -3,10 +3,12 @@ echo "Welcome"
 read -p "Enter 1st value=" a;
 read -p "Enter 2st value=" b;
 read -p "Enter 3st value=" c;
-opration1=`awk "BEGIN {print $a+($b*$c)}"`
-echo "The output for Opration-1 = "$opration1
-opration2=`awk "BEGIN {print ($a*$b)+$c}"`
-echo "The output for Opration-2 = "$opration2
-opration3=`awk "BEGIN{print $c+($a/$b)}"`
-echo "The output for Opration-3 = "$opration3
+operation1=`awk "BEGIN {print $a+($b*$c)}"`
+echo "The output for Opration-1 = "$operation1
+operation2=`awk "BEGIN {print ($a*$b)+$c}"`
+echo "The output for Opration-2 = "$operation2
+operation3=`awk "BEGIN{print $c+($a/$b)}"`
+echo "The output for Opration-3 = "$operation3
+operation4=`awk "BEGIN{print $a%($b+$c)}"`
+echo "The output for Operation-3 = "$operation4
 
